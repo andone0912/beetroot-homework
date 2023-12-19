8d0776b79f31abd56e9b3f087576533d4a97c55c
Andone0912 <andonedarius04@gmail.com>
2023-12-19 20:36:58 +0200

    added various stuff

---
+++ react/testReact.js
@@ -0,0 +1,283 @@
+function Hello() {
+  return (
+        <div>
+          <div className="top-header">
+              <div className="top-header-left">
+                  <div className="first-text"><img className="box-photo" src="images/box-logo.png"/>LIVRARE GRATUITĂ PESTE 450 RON
+                  </div>
+                  <div className="second-text"><img className="return-photo" src="images/return-logo.png"/> RETUR ÎN 14 ZILE</div>
+                  <div className="third-text"><img className="time-photo" src="images/24h-logo.png"/>LIVRARE ÎN 24H - 48H</div>
+              </div>
+              <div className="top-header-right">
+                  <div className="search-bar"><i className="fa-solid fa-magnifying-glass"></i>CAUTĂ</div>
+                  <div><i className="fa-regular fa-user"></i><span className="log-in-className">AUTENTIFICĂ-TE</span></div>
+                  <div className="shopping-cart"><i className="fa-solid fa-cart-shopping"></i>COȘ</div>
+              </div>
+          </div>
+          <div className="middle-header">
+              <a href="#" className="news-title">EASYBOX DELIVERY NOW AVAILABLE</a>
+          </div>
+          <div className="bottom-header">
+              <div className="bottom-header-items-alt"><img className="menu-button" src="images/menu-icon.png"/><span
+                      className="hidden-search-icon"><i className="fa-solid fa-magnifying-glass"></i></span></div>
+              <div className="bottom-header-items"><span className="invisible-numbers">5</span></div>
+              <div className="bottom-header-items"><a href="main.html"><img id="snkr-logo"
+                          src="images/sneaker-industry-photo.jpg"/></a></div>
+              <div className="bottom-header-items"><span className="invisible-numbers">5</span></div>
+              <div className="bottom-header-items-list">
+                  <div className="right-menu">
+                      <div className="basic-color"><a href="https://www.facebook.com/">SNEAKERS MEN</a></div>
+                      <div className="basic-color"><a href="#">SNEAKERS WOMEN</a></div>
+                      <div className="basic-color"><a href="#">CLOTHING</a></div>
+                      <div className="basic-color"><a href="#">ACCESORIES</a></div>
+                      <div className="alternate-color"><a href="#">SALES</a></div>
+                  </div>
+  
+              </div>
+              <div className="bottom-header-items-changing-nav">
+                  <div className="search-bar-2"><i className="fa-solid fa-magnifying-glass"></i><span
+                          className="second-nav-text second-nav-text-search">CAUTĂ</span>
+                  </div>
+                  <div className="log-in-className-2"><i className="fa-regular fa-user"></i><span
+                          className="second-nav-text">AUTENTIFICĂ-TE</span></div>
+                  <div className="shopping-cart-2"><i className="fa-solid fa-cart-shopping"></i><span
+                          className="second-nav-text">COȘ</span></div>
+              </div>
+          </div>
+      <div className="photo-true-container">
+          <div className="middle-photo-container">
+              <div className="in-photo-section">
+                  <div className="middle-photo-container-title">SAMEDAY EASYBOX</div>
+                  <div className="middle-photo-container-title middle-photo-container-subtitle">ORDER AT THE CLOSEST EASYBOX
+                      LOCKER</div>
+                  <div className="middle-photo-container-title middle-photo-container-button"><button
+                          className="shop-now-button">SHOP NOW</button></div>
+              </div>
+          </div>
+      </div>
+      <div className="hidden-photo-container">
+          <div className="hidden-secondary-photo-container">
+              <div className="hidden-photo-title">
+                  <div className="hidden-text hidden-text-title">SAMEDAY EASYBOX</div>
+                  <div className="hidden-text hidden-text-subtitle">ORDER AT THE CLOSEST EASYBOX LOCKER</div>
+                  <div className="hidden-text hidden-text-button"><span className="hidden-shop-now-button">SHOP NOW</span></div>
+  
+              </div>
+          </div>
+      </div>
+      <div className="categories-container">
+  
+          <div className="gridbox-content">
+              <img className="category-images" src="images/sneaker-barbati.jpg"/>
+              <div className="category-text">
+                  <div>sneakers barbati</div>
+                  <div className="category-text-link">cumpara acum</div>
+              </div>
+          </div>
+  
+          <div className="gridbox-content">
+              <img className="category-images" src="images/sneakers-femei.jpg"/>
+              <div className="category-text">
+                  <div> sneakers femei</div>
+                  <div className="category-text-link">cumpara acum</div>
+              </div>
+          </div>
+  
+          <div className="gridbox-content">
+              <img className="category-images" src="images/imbracaminte.jpg"/>
+              <div className="category-text">
+                  <div>imbracaminte</div>
+                  <div className="category-text-link">cumpara acum</div>
+              </div>
+          </div>
+  
+          <div className="gridbox-content">
+              <img className="category-images" src="images/accesori.jpg"/>
+              <div className="category-text">
+                  <div>accesorii</div>
+                  <div className="category-text-link">cumpara acum</div>
+  
+              </div>
+          </div>
+  
+  
+      </div>
+      <div className="brands-container">
+          <div className="brands-title"><a href="#">BRAND-URI POPULARE</a></div>
+          <div className="brands-gridbox-container">
+              <div className="brand-item memo"><a href="#"><img className="brand-img" src="images/first-brand-memo.jpg"/></a></div>
+              <div className="brand-item notrainproof"><a href="#"><img className="brand-img"
+                          src="images/second-notrainproof.jpg"/></a></div>
+              <div className="brand-item converse"><a href="#"><img className="brand-img" src="images/third-converse.jpg"/></a>
+              </div>
+              <div className="brand-item crocs"><a href="#"><img className="brand-img" src="images/fourth-crocs.jpg"/></a></div>
+              <div className="brand-item jordan"><a href="#"><img className="brand-img" src="images/5th-jordan.jpg"/></a></div>
+              <div className="brand-item kami"><a href="#"><img className="brand-img" src="images/6th-kami.jpg"/></a></div>
+              <div className="brand-item huf"><a href="#"><img className="brand-img" src="images/7th-huf.jpg"/></a></div>
+              <div className="brand-item adidas"><a href="#"><img className="brand-img" src="images/8th-adidas.jpg"/></a></div>
+  
+  
+          </div>
+      </div>
+      <div className="latest-products-container">
+  
+          <div className="clearfix-container-products">
+  
+              <div className="latest-products-title">
+                  <span className="latest-products-subtitle">CHECK OUR</span>
+                  <span className="latest-products-text">LATEST PRODUCTS</span>
+                  <span className="latest-products-button">SHOP NOW</span>
+              </div>
+              <div className="latest-products-flexbox-container">
+                  <div className="latest-products-items first-latest-item">
+                      <div className="hidden-overlay">
+                          <span className="hidden-latest-products-subtitle">CHECK OUR</span>
+                          <span className="hidden-latest-products-text">LATEST PRODUCTS</span>
+                          <span className="hidden-latest-products-button">SHOP NOW</span>
+                      </div>
+                      <img src="images/latest-prod-1.jpg"/>
+                  </div>
+                  <div className="latest-products-items second-latest-item"><img src="images/latest-prod-2.jpg"/></div>
+              </div>
+          </div>
+      </div>
+      <div className="latest-products-container-2">
+  
+          <div className="latest-products-collections">
+              <div className="latest-products-items-2 third-latest-item"><img src="images/latest-prod-3.jpg"/>
+                  <div className="latest-products-overlay-text">
+                      <span className="latest-products-overlay-main-text">NOUA COLECTIE CARHARTT</span>
+                      <span className="latest-products-overlay-button">SHOP NOW</span>
+                  </div>
+              </div>
+              <div className="latest-products-items-2 fourth-latest-item"><img src="images/latest-prod-4.jpg"/>
+                  <div className="latest-products-overlay-text">
+                      <span className="latest-products-overlay-main-text">NOUA COLECTIE KARL KANI</span>
+                      <span className="latest-products-overlay-button">SHOP NOW</span>
+                  </div>
+              </div>
+  
+  
+          </div>
+  
+  
+  
+      </div>
+      <div className="best-sellers-container">
+          <div className="best-sellers-title">BEST SELLERS</div>
+  
+          <div className="best-sellers-grid-container">
+              <div className="grid-item grid-item-1">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/first-photo-best-sellers.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">JORDAN</p>
+                  <p className="link-handle-best-sellers"><a href="#">W 1 High OG</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 36.5, 37.5, 38, 38.5, 39, 40, 40.5, 41, 42, 42.5, 43,
+                      44, 44.5</p>
+                  <p>989,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-2">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/2nd-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">NEW BALANCE</p>
+                  <p className="link-handle-best-sellers"><a href="#">2002R</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 41.5, 42, 42.5, 43, 44, 44.5, 45</p>
+                  <p>799,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-3">
+                  <div className="grid-item-image">
+                      <span className="sale-notice-best-sellers">40%@CHECKOUT</span>
+                      <a href="#"><img src="images/best seller photos/3rd-photo-bst-sllr.jpg"/></a>
+                  </div>
+                  <p className="brand-name-best-sellers">NIKE</p>
+                  <p className="link-handle-best-sellers"><a href="#">Air Force 1 Low Retro</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 38.5, 41, 43, 44, 44.5, 45, 45.5, 46, 47</p>
+                  <p>729,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-4">
+                  <div className="grid-item-image">
+                      <span className="sale-notice-best-sellers">40%@CHECKOUT</span>
+                      <a href="#"><img src="images/best seller photos/4th-photo-bst-sllr.jpg"/></a>
+                  </div>
+                  <p className="brand-name-best-sellers">NIKE</p>
+                  <p className="link-handle-best-sellers"><a href="#">W Dunk Low</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 36.5, 37.5, 38.5, 40</p>
+                  <p>649,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-5">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/5th-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">NIKE</p>
+                  <p className="link-handle-best-sellers"><a href="#">Air Force 1 07</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 41, 42, 42.5, 43, 44, 44.5, 45, 45.5, 46</p>
+                  <p>659,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-6">
+                  <div className="grid-item-image">
+                      <span className="sale-notice-best-sellers">30%@CHECKOUT</span>
+                      <a href="#"><img src="images/best seller photos/6th-photo-bst-sllr.jpg"/></a>
+                  </div>
+                  <p className="brand-name-best-sellers">NIKE</p>
+                  <p className="link-handle-best-sellers"><a href="#">W Dunk Low Disrupt 2</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 36, 37.5, 38, 38.5, 39, 40, 40.5, 41, 42, 42.5, 43</p>
+                  <p>649,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-7">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/7th-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">CROCS</p>
+                  <p className="link-handle-best-sellers"><a href="#">Classic Marbled Clog</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 37-38, 38-39, 39-40, 41-42, 42-43, 43-44</p>
+                  <p>269,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-8">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/8th-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">NEW BALANCE</p>
+                  <p className="link-handle-best-sellers"><a href="#">2002R</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 41.5, 42, 42.5, 43, 44, 44.5, 45, 45.5</p>
+                  <p>849,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-9">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/9th-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">PUMA</p>
+                  <p className="link-handle-best-sellers"><a href="#">Slipstream</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 40, 40.5, 41, 42, 42.5, 43, 44, 44.5, 45</p>
+                  <p>559,00 RON</p>
+              </div>
+              <div className="grid-item grid-item-10">
+                  <div className="grid-item-image"><a href="#"><img
+                              src="images/best seller photos/10th-photo-bst-sllr.jpg"/></a></div>
+                  <p className="brand-name-best-sellers">JORDAN</p>
+                  <p className="link-handle-best-sellers"><a href="#">W Sophia Slide</a></p>
+                  <p className="size-options-best-sellers">Disponibil: 36.5, 39, 40.5</p>
+                  <p>399,00 RON</p>
+              </div>
+          </div>
+  
+  
+      </div>
+      <div className="about-container">
+          <div className="clearfix-about-container">
+              <p className="about-title">MAGAZIN SNEAKERS</p>
+              <p className="about-text">Sneaker Industry aduce în același loc adidași, sneakers, bocanci – mai exact, tot ce
+                  ține de footwear, articole vestimentare ale celor mai noi colecții și numeroase accesorii - toate
+                  adunate într-un magazin sport online!</p>
+              <p className="secondary-about-title">CELE MAI COOL ARTICOLE DE STREETWEAR – FII UP-TO-DATE CU SNEAKER INDUSTRY
+              </p>
+              <p className="secondary-about-text">Dacă ești în căutarea unui magazin sport, București sau Cluj sunt două
+                  dintre orașele în care Sneaker Industry are locații fizice. Pasionații pot vedea detaliile fiecărui
+                  model de sneaker și se pot bucura de o experiență diferită într-un magazin special destinat
+                  streetwear-ului.</p>
+          </div>
+      </div>
+      </div>
+  
+  
+ )
+}
+
+const container = document.getElementById('mydiv');
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />)
--- scripts-lessons/lesson13.js
+++ scripts-lessons/lesson13.js
@@ -94,6 +94,65 @@ return digits
 document.getElementById('number-digits').innerText = calculateDigits()
 }
 
-const numbers = '-1 2 3 4 5 7 8 9 1 -6';
+function decideNumbers() {
+const numbers = document.querySelector('#ten-numbers').value;
+let result = [...new Set(numbers.split(" ").map(Number))];
+let negative = []
+let positive = []
+let zero = []
+let odd = []
+let even = []
+for (let i = 0; i < result.length; i++){
+  if(result[i] > 0){
+    positive.push(result[i])
+  } else if ( result[i] === 0) {
+    zero.push(result[i])
+  } else if( result[i] < 0) {
+    negative.push(result[i])
+  }
+}
 
+for (let i = 0; i < result.length; i++){
+  if(result[i] % 2 === 0) {
+    even.push(result[i])
+  } else {
+    odd.push(result[i])
+  }
+}
 
+document.querySelector('#positive').innerHTML = positive;
+document.querySelector('#negative').innerHTML = negative;
+document.querySelector('#odd').innerHTML = odd;
+document.querySelector('#even').innerHTML = even;
+document.querySelector('#zero').innerHTML = zero;
+}
+
+function randomCalculation() {
+const n1 = document.querySelector('#n1').value;
+const symbol = document.querySelector('#symbol').value
+const n2 = document.querySelector('#n2').value;
+const result =`${n1} ${symbol} ${n2}`;
+const realResult = eval(result)
+document.querySelector('.answer').innerHTML = `${result} = ${realResult}` 
+document.querySelector('#n1').value = ''
+document.querySelector('#symbol').value = ''
+document.querySelector('#n2').value = ''
+}
+
+function moveNumbers() {
+  const n1 = document.querySelector('#list').value;
+  const moveBy = Number(document.querySelector('#move-by').value);
+  console.log(document.querySelector('#list').value);
+  let arr = []
+  arr = n1.toString().split('')
+  const numbersToMove = arr.slice(0, moveBy);
+  const numbersLeft = arr.slice(moveBy, arr.length)
+  for( let i = 0; i < numbersToMove.length; i++) {
+    numbersLeft.push(numbersToMove[i])
+  }
+  console.log(numbersLeft)
+  document.querySelector('.oldList').innerHTML = `${arr}`
+  document.querySelector('.newList').innerHTML = `${numbersLeft}`
+}
+const h = document.querySelector('#calc')
+//h.addEventListener("click", moveNumbers)
